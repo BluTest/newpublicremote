0d49b70885284ce8fb47250ad5a723cdb16f8e6b
benchex <135388617+benchex@users.noreply.github.com>
2024-11-13 15:54:48 -0800

    Update somefile.sh

--- somefile.sh
+++ somefile.sh
@@ -1,3 +1,3 @@
 is_this_a_password = "9876khg()&*%"
 what_about_this_password = "09786LKJGHiuytr(&^$%kKJYHG"
-AUTH_STRING=12a7d9a7e6d7e37a681fa9a34602396cbf34d577
+AUTH_TOKEN=12a7d9a7e6d7e37a681fa9a34602396cbf34d577
